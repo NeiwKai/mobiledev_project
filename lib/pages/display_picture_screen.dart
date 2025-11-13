bb33b1f5abbab4c38d2b9d1085ba7cacc3afb965
Suppanat Timngarm <111691144+NeiwKai@users.noreply.github.com>
2025-11-13 12:00:46 +0700

    display the image and its label

--- lib/pages/display_picture_screen.dart
+++ lib/pages/display_picture_screen.dart
@@ -6,14 +6,36 @@ import 'package:flutter/material.dart';
 
 class DisplayPictureScreen extends StatelessWidget {
   final String imagePath;
+  final String label;
 
-  const DisplayPictureScreen({super.key, required this.imagePath});
+  const DisplayPictureScreen({super.key, required this.imagePath, required this.label});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Display the Picture')),
-      body: Image.file(File(imagePath)),
+      //body: Image.file(File(imagePath)),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          // The captured image
+          Image.file(
+            File(imagePath),
+            fit: BoxFit.contain,
+          ),
+
+          const SizedBox(height: 5),
+
+          // The caption/label below the image
+          Text(
+            label,
+            style: const TextStyle(
+              fontSize: 10,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
